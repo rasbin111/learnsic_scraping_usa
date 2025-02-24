0e6def7741f1becee4d1920a4ce1d700c0c844ad
rasbin <rasbin.thapa@learnsic.com>
2025-02-24 14:05:37 +0545

    minor updates

--- core/spiders/1_university_of_wiscinson_superior.py
+++ core/spiders/1_university_of_wiscinson_superior.py
@@ -4,6 +4,7 @@ import re
 
 
 from itemloaders import ItemLoader
+from w3lib.html import remove_tags
 from core.items import CustomItem
 
 
@@ -21,6 +22,11 @@ class UOWSSPider(scrapy.Spider):
         courses = response.xpath("//div[contains(@class, 'program-item')]")
         for course in courses:
             course_info = {}
+            if "undergraduate" in response.url:
+                course_info["degree"] = "Undergraduate"
+            elif "graduate" in response.url:
+                course_info["degree"] = "Postgraduate"
+
             url = course.xpath(".//h3/a/@href").get()
             study_mode = course.xpath(".//span[contains(., 'Delivery')]/strong/text()").get()
             if study_mode:
@@ -37,13 +43,15 @@ class UOWSSPider(scrapy.Spider):
         for key, value in course_info.items():
             il.add_value(key, value)
         
+        il.add_xpath("course_name", "//h1")
         il.add_value("course_website", response.url)
-        duration = response.xpath("//a[contains(., 'Sample') and contains(., 'Plan')]/text()")
+
+
+        duration = response.xpath("//a[contains(., 'Sample') and contains(., 'Plan')]/text()").get()
         if duration:
             il.add_value("duration_raw", duration)
             il.add_value("duration", duration)
             il.add_value("duration_term", duration)
-        il.add_xpath("course_name", "//h1")
 
         il.add_xpath("course_des", "//div[contains(., 'Overview')] | //div[contains(@id, 'overview')]")
         il.add_xpath("course_des_html", "//div[contains(., 'Overview')] | //div[contains(@id, 'overview')]")
@@ -51,16 +59,40 @@ class UOWSSPider(scrapy.Spider):
         il.add_xpath("career", "//h2[contains(@id, 'career')]/following-sibling::*")
         il.add_xpath("career_html", "//h2[contains(@id, 'career')]/following-sibling::*")
 
-        program_url = response.xpath("//a[contains(., 'Required Courses')]/@href")
+        il.add_xpath("course_struct", "//div[contains(@id, 'curriculum')]")
+        il.add_xpath("course_struct_html", "//div[contains(@id, 'curriculum')]")
+
+        il.add_value("intake_month", "July, November")
+        il.add_value("intake_day", "July 1, November 15")
+
+        try:
+            degree = course_info["degree"]
+
+        except Exception as e:
+            degree = ""
+
+        study_mode = course_info["study_mode"]
+        if study_mode:
+            if ("Online" in study_mode and "On-Campus" not in study_mode) and "Undergraduate" in degree:
+                # pass
+                il.add_value("ielts_aoverall", "6.5")
+                il.add_value("toefl_ibt_aoverall", "80")
+                il.add_value("duolingo_aoverall", "115")
+            
+            else:
+                il.add_value("ielts_aoverall", "5.5")
+                il.add_value("pte_aoverall", "47")
+                il.add_value("toefl_pbt_aoverall", "500")
+                il.add_value("toefl_ibt_aoverall", "61")
+                il.add_value("duolingo_aoverall", "100")
+
+
+        # program_url = response.xpath("//a[contains(., 'Required Courses')]/@href")
+        yield il.load_item()
 
         
 
-        yield response.follow(program_url, callback=self.parse2, meta = {
-            "il": il
-            })
 
-    def parse2(self, response):
-        il = response.meta.get("il")
 
         
 
